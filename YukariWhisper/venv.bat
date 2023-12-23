b9a3c06ba152d70e09f5157978dbd82e54d2ef69
tyapa0@gmail.com <qpvf6dmjv26bat4>
2023-12-23 21:52:12 +0900

    fix venv.bat char code

--- YukariWhisper/venv.bat
+++ YukariWhisper/venv.bat
@@ -1,10 +1,10 @@
-ECHO セットアップを開始します。数十分かかります。しばらくお待ちください
+ECHO Start setup. It will take several minutes. Please wait
 set temp=../tmp
 python -m pip install --upgrade pip
 pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118
 pip install -r requirements.txt
 
-ECHO セットアップが完了しました。run.batを実行してください
+ECHO Setup is complete. Please run run.bat
 cd ../
 PAUSE
 deactivate
