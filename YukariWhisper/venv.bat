400ad15c47f212f82af62bdbf7703725f66341f3
tyapa0@gmail.com <qpvf6dmjv26bat4>
2024-02-23 15:57:05 +0900

    Fixed an issue where the version tag was lost in Faster-whisper side mistake, causing new installations to fail.

--- YukariWhisper/venv.bat
+++ YukariWhisper/venv.bat
@@ -1,7 +1,7 @@
 ECHO Start setup. It will take several minutes. Please wait
 set temp=../tmp
 python -m pip install --upgrade pip
-pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118
+pip install torch==2.1.2 torchvision==0.16.2 torchaudio==2.1.2 --index-url https://download.pytorch.org/whl/cu118
 pip install -r requirements.txt
 
 ECHO Setup is complete. Please run run.bat
